98efa39e2d3eb5cf5fd8cda773f598e159ca715b
JPZ <jeanpaulzune@gmail.com>
2017-11-07 22:53:57 -0500

    Need to figure out how to import API without pushing to git

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+// import API_KEY from ./app-env.ja;
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.API_KEY;
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce
 // some html
