3cb254c118ff94550a88fc8936cf8c523bb00f34
JPZ <jeanpaulzune@gmail.com>
2017-11-08 21:57:29 -0500

    Using props

--- src/index.js
+++ src/index.js
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import {API_KEY} from '../keys.js';
-
+import VideoList from './components/video_list';
 
 // Create a new component. This component should produce
 // some html
 class App extends Component {
   constructor(props) {
+    super(props);
 
-  this.state = { videos: [] };
+    this.state = { videos: [] };
 
-  YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
     // This will update the list with a new list of videos
-    this.state({ videos });
     // Syntactic sugar.ES6 allows to compact the code, when key and property are the same variable name
     // this.state({ videos: videos}); is the sames as this.state({ videos });
-    });
-  }
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      });
+    }
   render () {
   return (
+    // Passing prompt videos to VideoList.
+    // App is a parent component to VideoList so we need to pass the data also know as passing prompt.
+    // This is jsx syntatic btw
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
     );
   }
