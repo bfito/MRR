3fc770373219510c10c11d01cb73cc4f763f7eaa
JPZ <jeanpaulzune@gmail.com>
2017-11-10 22:24:27 -0500

    List Item Keys

--- src/index.js
+++ src/index.js
@@ -13,19 +13,13 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    // This will update the list with a new list of videos
-    // Syntactic sugar.ES6 allows to compact the code, when key and property are the same variable name
-    // this.state({ videos: videos}); is the sames as this.state({ videos });
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos });
       });
     }
   render () {
   return (
-    // Passing prompt videos to VideoList.
-    // App is a parent component to VideoList so we need to pass the data also know as passing prompt.
-    // This is jsx syntatic btw
-    <div>
+      <div>
       <SearchBar />
       <VideoList videos={this.state.videos} />
     </div>
@@ -33,6 +27,5 @@ class App extends Component {
   }
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
