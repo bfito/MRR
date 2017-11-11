443d131fa4f5809e0336e66837e92639bc62e38a
JPZ <jeanpaulzune@gmail.com>
2017-11-11 09:53:46 -0500

    Video selection with two level callback

--- src/index.js
+++ src/index.js
@@ -11,21 +11,28 @@ import VideoDetail from './components/video_detail';
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
       YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+        });
       });
     }
 
   render () {
     return (
       <div>
-      <SearchBar />
-      <VideoDetail video={this.state.videos[0]}/>
-      <VideoList videos={this.state.videos} />
-    </div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
     );
   }
 }
